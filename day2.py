269e0c9bbcf8cce9f6cfc018780935c57d32fab8
Michele Dussin <87281404+Duss02@users.noreply.github.com>
2023-12-02 11:49:06 +0100

    Update day2.py

--- day2.py
+++ day2.py
@@ -37,3 +37,41 @@ for game in data_input.split('\n'):
         possible_games_sum += game_number
 
 print(possible_games_sum)
+
+
+#---------------------------------------------------------------------------------------
+#PART TWO
+
+sumpower = 0
+#ogni game
+for game in data_input.split('\n'):
+  
+    game_info = game.split(':')
+    game_number = int(game_info[0].split()[1])
+    sets_data = game_info[1].split(';')
+    possible_game = True
+    
+    temp_red = 0
+    temp_blue = 0 
+    temp_grern = 0
+    #ogni set
+    for set_data in sets_data:
+        elements = set_data.strip().split(', ')
+        #controlla elementi
+        for element in elements:
+            parts = element.split()
+            color = parts[1]
+            count = int(parts[0])
+            #in ogni set 
+            if (color == 'red' and count > temp_red):
+                temp_red = count
+            if (color == 'green' and count > temp_grern):
+                temp_grern = count
+            if (color == 'blue' and count > temp_blue):
+                temp_blue = count
+        
+    power =  temp_blue * temp_grern * temp_red   
+    sumpower += power
+
+
+print(sumpower)
